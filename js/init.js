d301ef8721883cd28dfd15958f8917433b204154
banyapon <daydevthailand@gmail.com>
2022-07-09 09:15:21 +0700

    first commit

---
+++ js/init.js
@@ -0,0 +1,11 @@
+var config = {
+  apiKey: "AIzaSyCVmOHgymUK1WnQlyvzTw9feL-0eLkxZbk",
+  authDomain: "doasialabs.firebaseapp.com",
+  projectId: "doasialabs",
+  storageBucket: "doasialabs.appspot.com",
+  messagingSenderId: "30464322780",
+  databaseURL: "https://doasialabs-default-rtdb.firebaseio.com",
+  appId: "1:30464322780:web:80b62f289b3c0d4df91819",
+  measurementId: "G-C2K97R0D3H"
+};
+firebase.initializeApp(config);
